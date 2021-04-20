c39219b842b5577f6fa3858bca299e90206637d5
Nastez <mineevanastez@gmail.com>
2021-04-20 12:15:16 +0300

    Added button for saving preview in png

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import './App.module.css'
-import React from 'react'
+import React, {useRef} from 'react'
 import Preview from './components/Preview/Preview'
 import s from './App.module.css'
 import ParameterInputForm from './components/ParameterForm/ParameterInputForm'
 import {useSelector} from 'react-redux'
 import {getColorFill, getGradientFill} from './redux/preview-selector'
-
+import domtoimage from 'dom-to-image'
+import {saveAs} from 'file-saver'
 
 const App: React.FC = () => {
 
     const colorFill = useSelector(getColorFill)
     const gradientFill = useSelector(getGradientFill)
 
+    const captureRef = useRef(null)
+
+    const saveInPNG = () => {
+        const image = captureRef.current
+
+        domtoimage.toBlob(image!)
+            .then(function (blob) {
+                saveAs(blob, 'my-node.png')
+            })
+
+    }
+
     return (
         <div className={s.wrapper}>
-            <div className={s.preview}
-                 style={{backgroundColor: colorFill, background: `linear-gradient(${gradientFill})`}}>
+            <div
+                id='img'
+                className={s.preview}
+                style={{backgroundColor: colorFill, background: `linear-gradient(${gradientFill})`}}
+                ref={captureRef}>
                 <Preview/>
             </div>
             <ParameterInputForm/>
             <div>
-                <button>Save in png</button>
+                <button onClick={saveInPNG}>Save in png</button>
                 <button>Copy (html or jsx)</button>
                 <button>Copy in json</button>
             </div>
--- src/components/Preview/PreviewImg/PreviewImg.tsx
+++ src/components/Preview/PreviewImg/PreviewImg.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import s from '../Preview.module.css'
 import {useSelector} from 'react-redux'
 import {getImgLink} from '../../../redux/preview-selector'
 
 const PreviewImg: React.FC = () => {
-
-    const imgLink = useSelector(getImgLink)
+    let imgLink = useSelector(getImgLink)
 
     return <div>
-        <img src={imgLink}/>
+        <img src={imgLink} />
     </div>
 }
 
 export default PreviewImg
+
+
